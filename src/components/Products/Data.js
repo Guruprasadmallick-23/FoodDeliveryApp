a27c96fce66f967a3b68770d64b147a700cd362c
Guruprasadmallick-23 <guruprasadmallick333@gmail.com>
2021-09-23 21:09:15 +0530

    Add :Data of the products available

---
+++ src/components/Products/Data.js
@@ -0,0 +1,106 @@
+import product1 from '../../images/burger.png';
+import product2 from '../../images/noodles.png';
+import product3 from '../../images/pizza.png';
+import product4 from '../../images/Cake.jpg';
+import product5 from '../../images/sweet.jpg';
+import product6 from '../../images/icecream.jpg';
+import product7 from '../../images/icecream.jpg';
+import product8 from '../../images/icecream.jpg';
+import product9 from '../../images/icecream.jpg';
+import product10 from '../../images/icecream.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'burger',
+    name: 'Supreme Burger',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$100Rs',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'noodles',
+    name: 'Chinese Noodles',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$80Rs',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Chicken Pizza',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$250Rs',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: product4,
+    alt: 'Cake',
+    name: 'Chocolate Cake',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '$160Rs',
+    button: 'Add to Cart'
+  },
+  {
+    img: product5,
+    alt: 'Sweet',
+    name: 'Rosogolla',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '10Rs',
+    button: 'Add to Cart'
+  },
+  {
+    img: product6,
+    alt: 'icecream',
+    name: 'Milk Icecream',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '20Rs',
+    button: 'Add to Cart'
+  },
+  {
+    img: product7,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product8,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+   {
+    img: product9,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product10,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  }
+];
